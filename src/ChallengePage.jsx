7be2cccc57e42a7975b9eb09d75e94045b003f19
soorajsunil14 <sooraj.namboothiry@gmail.com>
2025-02-11 23:53:40 +0530

    Added props

--- src/ChallengePage.jsx
+++ src/ChallengePage.jsx
@@ -1,7 +1,7 @@
 import SpecialDiv from "./components/borderDiv";
 import AnimatedText from "./components/text";
 
-const ChallengePage = () => {
+const ChallengePage = ({ ctfName = "CTF NAME", ctfDescription = "" }) => {
 
     return (
         <div className="grid grid-rows-17 h-[100vh] w-[100vw] p-10 text-center font-neuebit">
@@ -17,13 +17,10 @@ const ChallengePage = () => {
                     <SpecialDiv className="flex items-center justify-center hover:bg-enigma-green hover:text-black cursor-pointer"><AnimatedText text="BACT TO CTFs" /></SpecialDiv>
                     <SpecialDiv className="flex items-center justify-center"><AnimatedText text="CTF CREATOR" /></SpecialDiv>
                 </SpecialDiv>
-                <SpecialDiv className="col-span-3 flex items-center justify-center text-[110px] text-enigma-green"><AnimatedText text="CTF NAME" /></SpecialDiv>
+                <SpecialDiv className="col-span-3 flex items-center justify-center text-[110px] text-enigma-green"><AnimatedText text={ctfName} /></SpecialDiv>
             </SpecialDiv>
             <SpecialDiv className="grid grid-cols-4 row-span-12 text-4xl">
-                <SpecialDiv className="col-span-3 p-10 text-start">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, nemo. Repudiandae cumque cum nemo, officiis obcaecati exercitationem. Corporis aliquid ex non dolorum incidunt est, aspernatur dignissimos totam a maxime consequuntur?
-                    At suscipit, nam, esse vitae quidem officiis vel molestias aperiam, voluptates maiores ipsam assumenda et corrupti recusandae! Doloremque omnis voluptatum consequatur ut non corrupti asperiores cum architecto dolor! Odio, eos!
-                    Ab consequatur nesciunt maxime quidem placeat aliquam cupiditate repellendus excepturi hic est deleniti vitae nisi sequi, nobis quibusdam ea praesentium deserunt quia recusandae voluptatibus dolore velit illo tenetur. Placeat, at!
-                    Non perferendis nemo consequuntur quae laboriosam voluptatum beatae ipsam veritatis facere numquam, repudiandae aliquid, soluta nulla unde. Excepturi, perferendis nobis! Accusamus modi sunt laudantium natus laboriosam eaque est, recusandae unde!</SpecialDiv>
+                <SpecialDiv className="col-span-3 p-10 text-start">{ctfDescription}</SpecialDiv>
                 <SpecialDiv className="grid grid-rows-6 col-span-1">
                     <div id="div" className="row-span-2 cursor-pointer">
                         <SpecialDiv className="h-full w-full bg-enigma-green text-black flex items-center justify-center flex-col">
